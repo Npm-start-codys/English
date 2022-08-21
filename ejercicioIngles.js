b650173713179e984e5ffeeba5730e48d78915ff
npm-start <110133115+freedevsteam@users.noreply.github.com>
2022-08-21 19:00:25 -0300

    Create ejercicioIngles.js

---
+++ ejercicioIngles.js
@@ -0,0 +1,23 @@
+// What a mess!! Order the animals and their features in a new array.
+//then translate the new array to spanish
+const animals = [
+  {
+    elephant: "blue",
+    legs: false,
+    feature: "swim",
+  },
+  { whale: "grey", legs: 4, feature: "strong trunk" },
+  { duck: "red", legs: false, feature: "crawl" },
+  { snake: "white", legs: 2 },
+];
+
+
+//What happened? put the house in order in a new array
+//then translate the names of the ordered arrays to spanish
+const bedroom = Array.of("bathtub", "toilet", "mirror", "shower");
+
+const kitchen = Array.of("bed","lamp", "pillow", "blanket", "closet" )
+
+const living = Array.of("table", "chair", "plate", "cup", "glass")
+
+const diningRoom = Array.of("sofa", "rug", "bookcase","picture","clock");
